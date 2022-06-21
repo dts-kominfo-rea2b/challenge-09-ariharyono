41234414b71b556cd6355017715b44322cf0ab2d
Ari Haryono <33080550+ariharyono@users.noreply.github.com>
2022-06-21 22:16:26 +0700

    percobaan ketiga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
+
 // Uncomment untuk memuat daftar kontak
 import contacts from './data/contacts.json';
 
